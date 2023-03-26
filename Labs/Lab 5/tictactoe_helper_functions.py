651e6f48241323f2ab28c13265be4874600198c2
Peter TA <pet2011@hotmail.dk>
2023-03-26 21:34:58 +0200

    Add lab5

---
+++ Labs/Lab 5/tictactoe_helper_functions.py
@@ -0,0 +1,17 @@
+class TicTacToe():
+
+    @staticmethod
+    def checkForAllWinOrTie(board):
+        pass
+
+    @staticmethod
+    def checkForVerticalWin(board):
+        pass
+
+    @staticmethod
+    def checkForHorizontalWin(Board):
+        pass
+
+    @staticmethod
+    def checkForDiagnolWin(Board):
+        pass
